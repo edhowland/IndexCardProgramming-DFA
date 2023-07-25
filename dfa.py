e3a88f140073081651b610dcb14cf12e84b6fb55
Ed Howland <ed.howland@gmail.com>
2023-07-24 20:10:50 -0400

    working dfa.py table.py

--- dfa.py
+++ dfa.py
@@ -1,7 +1,7 @@
-
+import sys
 from table import delta, isaccept
 
-i="abcd"
+i=sys.argv[1]
 s=0
 while len(i) != 0:
     c=i[0]; i=i[1:]
--- table.py
+++ table.py
@@ -1,6 +1,6 @@
 
 def delta(s, c):
-    return 0
+    return { (0, "a"): 1, (1, "b"): 2, (0, "c"): 3, (3, "d"): 4, (2, "a"): 1, (2, "c"): 3, (4, "a"): 1, (4, "c"): 3}.get((s, c), 999)
     
 def isaccept(s):
-    return False
+    return s in {2, 4}
